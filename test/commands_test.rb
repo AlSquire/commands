9717c5a0f3d6beaf257f509eefeaade7f130a098
Arnaud Lécuyer <alsquire@gmail.com>
2021-09-09 19:56:04 +0200

    Add test with commands.yml present

--- test/commands_test.rb
+++ test/commands_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
+require "readline"
+require "mocha/minitest"
 
 class CommandsTest < Minitest::Test
+  def teardown
+    FileUtils.remove_file 'commands.yml' if File.exist? 'commands.yml'
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Commands::VERSION
   end
@@ -10,6 +16,17 @@ class CommandsTest < Minitest::Test
       load './exe/commands'
     end
 
-    assert_match /Example of commands.yml/, out
+    assert_match "Example of commands.yml", out
+  end
+
+  def test_it_outputs_interactive_menu_when_commands_yml_is_present
+    FileUtils.cp './test/commands.sample.yml', 'commands.yml'
+
+    Readline.expects(:readline).returns('')
+    out, err = capture_io do
+      load './exe/commands'
+    end
+
+    assert_match "Enter a number or alias followed by optional args, nothing to exit", out
   end
 end
